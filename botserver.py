abc98fbcf92086291ff2d70c6722a26ae58db7a1
horden316 <horden316@gmail.com>
2023-04-24 01:32:12 +0200

    feat: Login Conformation finish

--- botserver.py
+++ botserver.py
@@ -8,13 +8,10 @@ AuthenticationList = [[23019, 32037], [32037, 29295],
                       [18789, 13603], [16443, 29533], [18189, 21952]]
 
 
-LoginStatus = 0
 # 0 means the user haven't done anything
 # 1 means got the CLIENT_USERNAME
 # 2 means got the CLIENT_KEY_ID
 # 4 means successful login
-
-UserName = ""
 
 
 def ServerStart():
@@ -39,12 +36,18 @@ def ServerStart():
         c.settimeout(10)
         try:
             # recv gets sequence of bytes -> decoding into string
+
+            LoginStatus = 0
+            UserName = ""
+            KeyID = 5
+
             while LoginStatus < 5:
                 data = c.recv(1024).decode()
                 if '\a\b' in data:
                     print("legal data")
                     data = data[:-2]
-                    Login(data, c)
+                    LoginStatus, UserName, KeyID = Login(
+                        data, c, LoginStatus, UserName, KeyID)
 
             c.close()
             break  # child executes only one cycle
@@ -54,28 +57,44 @@ def ServerStart():
             c.close()
 
 
-def Login(data, c):
+def Login(data, c, LoginStatus, UserName, KeyID):
     print("Full data is : "+data)
-    global LoginStatus
-    global UserName
     global AuthenticationList
     if LoginStatus == 0:
         UserName = data
         print("UserName is:" + UserName)
         c.send("107 KEY REQUEST\a\b".encode())  # Sending SERVER_KEY_REQUEST
-        LoginStatus = 1
+        return 1, UserName, KeyID
 
-    elif LoginStatus == 1:  # handle with the CLIENT_KEY_ID
+    elif LoginStatus == 1:  # handle with CLIENT_KEY_ID
         data = int(data)
-        print("innnnnn")
+        KeyID = data
         NameASCI = [ord(char) for char in UserName]
         print("Name Ascii is : " + str(NameASCI))
         ResultHash = (sum(NameASCI)*1000 %
-                      65536 + int(AuthenticationList[data][0])) % 65536
+                      65536 + int(AuthenticationList[KeyID][0])) % 65536
         print("hash: " + str(ResultHash))
         # Sending SERVER_CONFIRMATION
         c.send((str(ResultHash)+"\a\b").encode())
-        LoginStatus = 2
+        return 2, UserName, KeyID
+
+    elif LoginStatus == 2:  # handle with CLIENT_CONFIRMATION
+        data = int(data)
+        NameASCI = [ord(char) for char in UserName]
+        print("Name Ascii is : " + str(NameASCI))
+        ResultHash = (sum(NameASCI)*1000 %
+                      65536 + int(AuthenticationList[KeyID][1])) % 65536
+        print("hash: " + str(ResultHash))
+        if (data == ResultHash):
+            # Sending SERVER_CONFIRMATION
+            c.send("200 OK\a\b".encode())
+        elif (data != ResultHash):
+            # Sending SERVER_CONFIRMATION
+            c.send("300 LOGIN FAILED\a\b".encode())
+
+        return 3, UserName, KeyID
+
+    return 5, UserName, KeyID
 
 
 if __name__ == '__main__':
