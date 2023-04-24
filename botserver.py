e7b98f94b090fc06f782230b3919f2802816d08d
horden316 <horden316@gmail.com>
2023-04-25 01:43:05 +0200

    feat: seem like works for ideal situation

--- botserver.py
+++ botserver.py
@@ -35,27 +35,23 @@ def ServerStart():
         print("Address is " + str(address))
         c.settimeout(10)
         try:
-            # recv gets sequence of bytes -> decoding into string
 
             LoginStatus = 0
             UserName = ""
             KeyID = 5
-            dir = 3
+            dir = 2
+            lastPos = [100, 100]
+            Inimove = 0  # first time to move
 
             while LoginStatus < 3:
-                data = c.recv(1024).decode()
-                if '\a\b' in data:
-                    print("legal data")
-                    data = data[:-2]
-                    LoginStatus, UserName, KeyID = Login(
-                        data, c, LoginStatus, UserName, KeyID)
+                data = RecieveData(c)
+                LoginStatus, UserName, KeyID = Login(
+                    data, c, LoginStatus, UserName, KeyID)
 
-            while LoginStatus == 3:
-                data = c.recv(1024).decode()
-                if '\a\b' in data:
-                    print("legal data")
-                    data = data[:-2]
-                    Navi(data, c, dir)
+            while LoginStatus == 3:  # Successful Login
+                data = RecieveData(c)
+                LoginStatus, lastPos, Inimove = Navi(
+                    data, c, LoginStatus, lastPos, Inimove)
 
             c.close()
             break  # child executes only one cycle
@@ -106,16 +102,116 @@ def Login(data, c, LoginStatus, UserName, KeyID):
     return 5, UserName, KeyID
 
 
-def Navi(data, c, dir):
+def Navi(data, c, LoginStatus, lastPos, Inimove):
+
+    position = GetPosition(data)
+
+    if Inimove == 0:  # move the robot to guess the robot position
+        print("hey1")
+        lastPos = position
+        c.send("102 MOVE\a\b".encode())
+        Inimove = 1
+        return LoginStatus, lastPos, Inimove
+
+    if (position[0] == 0 and position[1] == 0):
+        print("hey2")
+        c.send("105 GET MESSAGE\a\b".encode())
+        data = RecieveData(c)
+        print(data)
+        c.send("106 LOGOUT\a\b".encode())
+
+        LoginStatus = 4
+        # recieve message
+
+    elif position[0] == 0:  # pos on the y-asis
+        print("hey3")
+        if (position[0]-lastPos[0] != 0):
+            # while robot first arrive the y-asis turn right whatever
+            c.send("104 TURN RIGHT\a\b".encode())
+        if (abs(lastPos[1])-abs(position[1]) == 1):
+            lastPos = position
+            c.send("102 MOVE\a\b".encode())
+
+        elif (abs(lastPos[1])-abs(position[1]) == -1):
+            lastPos = position
+            c.send("104 TURN RIGHT\a\b".encode())
+            data = RecieveData(c)
+            c.send("104 TURN RIGHT\a\b".encode())
+            data = RecieveData(c)
+            c.send("102 MOVE\a\b".encode())
+
+    elif position[1] == 0:  # pos on the x-asis
+        print("hey4")
+        print("position"+str(position))
+        print("lastPos"+str(lastPos))
+        if (position[1]-lastPos[1] != 0):
+            # while robot first arrive the x-asis turn right whatever
+            c.send("104 TURN RIGHT\a\b".encode())
+        if (abs(lastPos[0])-abs(position[0]) == 1):
+            lastPos = position
+            c.send("102 MOVE\a\b".encode())
+
+        elif (abs(lastPos[0])-abs(position[0]) == -1):
+            lastPos = position
+            c.send("104 TURN RIGHT\a\b".encode())
+            data = RecieveData(c)
+            c.send("104 TURN RIGHT\a\b".encode())
+            data = RecieveData(c)
+            c.send("102 MOVE\a\b".encode())
+
+    else:  # pos not on the x or y-asis
+        if (abs(lastPos[0])-abs(position[0]) == -1):
+            lastPos = position
+            c.send("104 TURN RIGHT\a\b".encode())
+            data = RecieveData(c)
+            c.send("104 TURN RIGHT\a\b".encode())
+            data = RecieveData(c)
+            c.send("102 MOVE\a\b".encode())
+        elif (abs(lastPos[0])-abs(position[0]) == 1):
+            lastPos = position
+            c.send("102 MOVE\a\b".encode())
+
+        elif (abs(lastPos[1])-abs(position[1]) == 1):
+            lastPos = position
+            c.send("102 MOVE\a\b".encode())
+
+        elif (abs(lastPos[1])-abs(position[1]) == -1):
+            lastPos = position
+            c.send("104 TURN RIGHT\a\b".encode())
+            data = RecieveData(c)
+            c.send("104 TURN RIGHT\a\b".encode())
+            data = RecieveData(c)
+            c.send("102 MOVE\a\b".encode())
+
+    # if (position[0]-lastPos[0] == 0) and (position[1]-lastPos[1] == 0):  # obstactle solve
+    #     lastPos = position
+    #     c.send("104 TURN RIGHT\a\b".encode())
+    #     data = RecieveData(c)
+    #     c.send("103 TURN LEFT\a\b".encode())
+    #     data = RecieveData(c)
+    #     c.send("103 TURN LEFT\a\b".encode())
+    #     data = RecieveData(c)
+    #     c.send("104 TURN RIGHT\a\b".encode())
+    #     data = RecieveData(c)
+    #     c.send("102 MOVE\a\b".encode())
+
+    return(LoginStatus, lastPos, Inimove)
+
+
+def RecieveData(c):
+    data = c.recv(1024).decode()
+    if '\a\b' in data:
+        print("legal data")
+        data = data[:-2]
+        print("data is: "+str(data))
+    return data
+
+
+def GetPosition(data):
     input = data.split()
     position = [int(input[i]) for i in range(1, len(input))]
     print(position)
-    if position[1] > 0:
-        if dir == 0:
-            c.send("103 TURN LEFT\a\b".encode())
-            dir = 3
-        elif dir == 3:
-            c.send("102 MOVE\a\b".encode())
+    return position
 
 
 if __name__ == '__main__':
